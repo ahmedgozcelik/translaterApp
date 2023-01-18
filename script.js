72720a773d231362bee3dcb38a2e05158cf5cfc8
Ahmed Gürkan Özçelik <ahmetgozcelik@gmail.com>
2023-01-18 10:55:01 +0300

    Lists have been created and languages ​​to be translated have been added.

---
+++ script.js
@@ -0,0 +1,14 @@
+const fromLang = document.querySelector("#from-lang");
+const toLang = document.querySelector("#to-lang");
+
+for(let lang in languages){
+    // console.log(lang, languages[lang]);
+    let option = `
+        <option value="${lang}">${languages[lang]}</option>
+    `;
+    fromLang.insertAdjacentHTML("beforeend", option);
+    toLang.insertAdjacentHTML("beforeend", option);
+
+    fromLang.value = "tr-TR";
+    toLang.value = "en-GB";
+}
